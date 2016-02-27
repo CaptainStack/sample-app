4757ce963b2cb823f12581848bbdb49f4dab39c5
Andre Stackhouse <captainstack@outlook.com>
2016-02-27 04:20:56 +0000

    Progress through 11.3.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Andre Stackhouse",
+             email: "captainstack@outlook.com",
+             password:              "password",
+             password_confirmation: "password",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
@@ -16,4 +16,10 @@ User.create!(name:  "Example User",
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
